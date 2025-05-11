87db6ea16677905942abda3384d1367d45adce2e
YousefAhmed32 <my10424231@gmail.com>
2025-05-11 05:08:31 +0300

    Initial commit

---
+++ src/components/Newsletter.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { Col, Row, Alert } from "react-bootstrap";
+
+export const Newsletter = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState('');
+  const [showSuccess, setShowSuccess] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email && email.includes("@")) {
+
+      setShowSuccess(true);
+      clearFields();
+
+   
+      setTimeout(() => {
+        setShowSuccess(false);
+      }, 5000);
+    }
+  };
+
+  const clearFields = () => {
+    setEmail('');
+  };
+
+  return (
+    <Col lg={12}>
+      <div className="newsletter-bx wow slideInUp">
+        <Row>
+          <Col lg={12} md={6} xl={5}>
+            <h3>Subscribe to our Newsletter<br />& Never miss latest updates</h3>
+           
+            {showSuccess && <Alert variant="success">✅ Successfully subscribed!</Alert>}          </Col>
+          <Col md={6} xl={7}>
+            <form onSubmit={handleSubmit}>
+              <div className="new-email-bx">
+                <input
+                  value={email}
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email Address"
+                  required
+                />
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+          </Col>
+        </Row>
+      </div>
+    </Col>
+  );
+};
