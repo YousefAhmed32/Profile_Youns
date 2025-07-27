d1dae129c4f8a1374b9136be6918a71858554c6d
Yousef Ahmed <my10424231@gmail.com>
2025-07-27 11:05:25 +0300

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-// import headerImg from "../assets/img/header-img.svg";
 import headerImg from "../assets/img/img-me1-1.jpg";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
